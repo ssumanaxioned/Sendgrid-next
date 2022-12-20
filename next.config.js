58234e965e3c6a42b4949ac67436d7d6b3c0ca5f
ssumanaxioned <sumans@axioned.com>
2022-12-20 19:58:50 +0530

    added env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SENDER_GMAIL: process.env.SENDER_GMAIL,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  }
 }
 
 module.exports = nextConfig
